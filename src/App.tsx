f0c670f34a1b7653f5acc50a790d474036ddbd58
Álvaro Ferreira <alvaro.ferreira@amcom.com.br>
2023-02-08 10:04:58 -0300

    feat: new experiences

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,16 @@ import {
   AiFillPhone,
 } from "react-icons/ai";
 import { FaWhatsappSquare } from "react-icons/fa";
-import profile from "./assets/profile.jpg"
-import software from "./assets/software.png"
-import farmaceutica from "./assets/farmaceutica.jpg"
-import geolocalizacao from "./assets/geolocalizacao.jpg"
-import planoSaude from "./assets/plano-saude.jpg"
-import cloud from "./assets/cloud.png"
-import trophy from "./assets/trophy.png"
-
+import profile from "./assets/profile.jpg";
+import software from "./assets/software.png";
+import cloud from "./assets/cloud.png";
+import trophy from "./assets/trophy.png";
+import farmaceutica from "./assets/farmaceutica.jpg";
+import geolocalizacao from "./assets/geolocalizacao.jpg";
+import planoSaude from "./assets/plano-saude.jpg";
+import callCenter from "./assets/call-center.jpg";
+import whatsapp from "./assets/whatsapp.jpeg";
+import comercio from "./assets/comercio.jpg";
 
 function App() {
   return (
@@ -41,7 +43,6 @@ function App() {
           anos de experiencia como desenvolvedor fullstack e techlead focado
           principalmente em NodeJS e ReactJS.{" "}
         </p>
-
 
         <div className="text-5xl py-4 flex justify-center gap-10">
           <a
@@ -100,6 +101,7 @@ function App() {
               <li>ReactJS</li>
               <li>React Native</li>
               <li>NextJS</li>
+              <li>Typescript</li>
             </ul>
           </div>
 
@@ -108,24 +110,26 @@ function App() {
             <h2 className="text-xl">Backend, API's & Integrações</h2>
             <p className="text-gray-500">
               Também pensando em qualidade e escalabilidade, ao desenvolver os
-              backends sempre priorizo a seriedade para tratar de dasos e
-              seguranaça interna.
+              backends priorizo a descrição e responsabilidade para tratar de
+              dados e seguranaça interna dos clientes.
             </p>
             <p className="text-xl">Ferramentas que uso</p>
             <ul>
               <li>NodeJS</li>
               <li>NestJS</li>
               <li>Banco de dados SQL e noSQL</li>
+              <li>Typescript</li>
             </ul>
           </div>
 
           <div className="w-full shadow-lg rounded-md px-4 py-5 flex flex-col items-center text-center gap-1">
             <img src={trophy} className="w-48" alt="gambar" />
-            <h2 className="text-xl">PO & Scrum Master</h2>
+            <h2 className="text-xl">Gestão Ágil</h2>
             <p className="text-gray-500">
-              Tendo também experiencia com gestão ágil, acredito poder colabolar
-              não só com minhas atividades mas também auxiliar em aplicar fluxos
-              que otimizem os resultados.
+              Ao decorrer da carreira tive a oportunidade de trabalhar com Ágil
+              como scrum team, PO e scrum master. Acredito que tais habilidades
+              podem não só colaborar para a execução de minhas atividades, mas
+              também ajudar o time como um todo.
             </p>
             <p className="text-xl ">Ferramentas que uso</p>
             <ul>
@@ -143,20 +147,38 @@ function App() {
           Aqui estão alguns dos projetos que já tive a oportunidade de atuar
         </p>
 
-        <div className="flex flex-col md:flex-row gap-8 mt-4">
+        <div className="flex flex-col md:flex-row gap-8 mt-3">
           <div className="w-full md:w-2/6 shadow-md rounded ">
             <div className="h-48 md:h-56 rounded-t-md relative overflow-hidden">
-              <img src={geolocalizacao} />
+              <img src={planoSaude} />
             </div>
             <div className="p-4">
               <h1 className="text-xl font-medium">
-                ERP de gestão de frotas utilizando geolocalização.
+                Ferramentas para plano de saúde
               </h1>
               <p className="">
-                Aplicação que monitora e controla em tempo real a frota do
-                cliente (multinacional).
+                Manutenção e criação de ferramentas para um dos maiores planos
+                de saúde do brasil, incluindo projetos de stream como
+                atendimento de paciente remoto.
               </p>
-              <p className="text-sm text-gray-500 mt-20">2021</p>
+              <p className="text-sm text-gray-500 mt-5">2023</p>
+            </div>
+          </div>
+
+          <div className="w-full md:w-2/6 shadow-md rounded ">
+            <div className="h-48 md:h-56 rounded-t-md relative overflow-hidden">
+              <img src={whatsapp} />
+            </div>
+            <div className="p-4">
+              <h1 className="text-xl font-medium">
+                Portal de atendimento híbrido via whatsapp
+              </h1>
+              <p className="">
+                Saas de atendimento ao cliente podendo ser de forma automática
+                (bot), ou por atendente e setores.
+              </p>
+              <br />
+              <p className="text-sm text-gray-500 mt-2">2022</p>
             </div>
           </div>
 
@@ -174,24 +196,60 @@ function App() {
                 medicamentos.
               </p>
               <br />
-              <p className="text-sm text-gray-500 mt-5">2022</p>
+              <p className="text-sm text-gray-500">2022</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-8 mt-3">
+          <div className="w-full md:w-2/6 shadow-md rounded ">
+            <div className="h-48 md:h-56 rounded-t-md relative overflow-hidden">
+              <img src={geolocalizacao} />
+            </div>
+            <div className="p-4">
+              <h1 className="text-xl font-medium">
+                ERP de gestão de frotas utilizando geolocalização.
+              </h1>
+              <p className="">
+                Aplicação que monitora e controla em tempo real a frota do
+                cliente (multinacional).
+              </p>
+              <br />
+              <p className="text-sm text-gray-500">2021</p>
             </div>
           </div>
 
           <div className="w-full md:w-2/6 shadow-md rounded ">
             <div className="h-48 md:h-56 rounded-t-md relative overflow-hidden">
-              <img src={planoSaude} />
+              <img src={callCenter} />
             </div>
             <div className="p-4">
               <h1 className="text-xl font-medium">
-                Desenvolvimento de ferramentas variadas para plano de saúde
+                Teachlead de ERP de gestão de Call Center.
               </h1>
               <p className="">
-                Manutenção e criação de ferramentas para um dos maiores planos
-                de saúde do brasil, incluindo projetos de stream como
-                atendimento de paciente remoto.
+                ERP de controle e gestão de call center (ativo e passivo). Neste
+                projeto tive a oportunidade de trabalhar com integrações de Pabx
+                voip.
               </p>
-              <p className="text-sm text-gray-500">2023</p>
+              <p className="text-sm text-gray-500">2020 - 2021</p>
+            </div>
+          </div>
+
+          <div className="w-full md:w-2/6 shadow-md rounded ">
+            <div className="h-48 md:h-56 rounded-t-md relative overflow-hidden">
+              <img src={comercio} />
+            </div>
+            <div className="p-4">
+              <h1 className="text-xl font-medium">
+                Saas de automação comercial.
+              </h1>
+              <p className="">
+                ERP para empresas com controle de contas, estoque, notas, e
+                processos internos em geral.
+              </p>
+              <br />
+              <p className="text-sm text-gray-500 pt-7">2019 - 2020</p>
             </div>
           </div>
         </div>
@@ -199,20 +257,24 @@ function App() {
 
       <section id="footer" className="mt-5 rounded bg-black py-8 px-8">
         <div className="text-white md:text-center">
-          <h1 className="font-medium text-lg text-white text-center">Contact Me</h1>
-          <p className="text-center">Ficou com alguma dúvida, ou quer falar comigo?</p>
+          <h1 className="font-medium text-lg text-white text-center">
+            Contact Me
+          </h1>
+          <p className="text-center">
+            Ficou com alguma dúvida, ou quer falar comigo?
+          </p>
 
           <ul className="md:flex justify-center gap-10 mt-5 mb-5">
             <li className="flex justify-center gap-2">
-              <AiFillMail className="mt-1"/>
+              <AiFillMail className="mt-1" />
               <p>alvarojfjunior@gmail.com</p>
             </li>
             <li className="flex justify-center gap-2">
-              <AiFillEnvironment className="mt-1"/>
+              <AiFillEnvironment className="mt-1" />
               <p>Montes Claros, MG. Brasil</p>
             </li>
             <li className="flex justify-center gap-2">
-              <AiFillPhone className="mt-1"/>
+              <AiFillPhone className="mt-1" />
               <p>+55 38 99161-1042</p>
             </li>
           </ul>
